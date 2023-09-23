74e71dc8afcdc9514b176eba6f336d61829aa8e0
Andrey.Skripay <lordktan@gmail.com>
2023-09-23 21:25:26 +0500

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         // Задача 1
+
+        int clientOS = 2;
+        switch (clientOS) {
+            case 1:
+                System.out.println("Установите версию приложения для Android по ссылке.");
+                break;
+            case 0:
+                System.out.println("Установите версию приложения для iOS по ссылке.");
+                break;
+            default:
+                System.out.println("Нет приложения для такой системы.");
+        }
     }
 }
